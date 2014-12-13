51418c3d9747461cfb7a1c6b6db3dac887b99118
Hattan Shobokshi <hattan@gmail.com>
2014-12-12 23:54:54 -0800

    lab2 changes

--- app.js
+++ app.js
@@ -1,63 +1,24 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 6500);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.get('/about',function(req,res){
-  res.render("about");
-});
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/',function(req,res){
+	res.send("hello world");
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.get('/users',function(req,res){
+  var users = [
+  	{name : 'bob smith', age : 35},
+  	{name : 'mike jones', age : 35},
+  	{name : 'tony stark', age : 35},
+  	{name : 'sally ride', age : 35}
+  ];
+  
+	res.json(users);
 });
 
-
-module.exports = app;
+var server = app.listen(app.get('port'),function(){
+	console.log("Server started on port " +server.address().port);
+})
