0c0789f3c5ac951fdb66067de60d3a994d7c72b3
Hattan Shobokshi <hattan@gmail.com>
2014-12-13 00:11:14 -0800

    lab3

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 var express = require('express');
+var routes = require('./routes/index');
 var path = require('path');
+var config =require('./config.js');
+var youtube = require('./youtube');
+var youTubeclient = new youtube.Client(config.youTube);
+
 var app = express();
 
 app.set('port', process.env.PORT || 6500);
 
-app.get('/',function(req,res){
-	res.send("hello world");
-});
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower',express.static(path.join(__dirname, 'bower_components')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+app.use('/',routes);
+app.get("/search",function(req,res){
+	var term = req.query['q'];
+	youTubeclient.search(term,function(data){
+		res.json(data);
+		res.end();
+	});
+});
 app.get('/users',function(req,res){
   var users = [
   	{name : 'bob smith', age : 35},
@@ -15,7 +29,7 @@ app.get('/users',function(req,res){
   	{name : 'tony stark', age : 35},
   	{name : 'sally ride', age : 35}
   ];
-  
+
 	res.json(users);
 });
 
---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	mongoUrl : '',
+	youTube : {
+		key : 'AIzaSyDDWf_ETIKJ-X48u1IELy4wHw5O4bj244E',
+		maxResults : 10		
+	},
+	facebook : {
+		clientID: '',
+		clientSecret : '',
+		callbackURL : 'http://localhost:3000/auth/facebook/callback'
+	}
+}
